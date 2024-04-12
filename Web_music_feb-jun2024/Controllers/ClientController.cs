00eedda78ad7cfd4809986646490266510b970f2
limef <limef@ACER_TATYANA>
2024-04-12 13:07:32 +0300

    Добавьте файлы проекта.

---
+++ Web_music_feb-jun2024/Controllers/ClientController.cs
@@ -0,0 +1,32 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using Web_music_feb_jun2024.Models;
+
+namespace Web_music_feb_jun2024.Controllers
+{
+    public class ClientController : Controller
+    {
+        private readonly ILogger<ClientController> _logger;
+
+        public ClientController(ILogger<ClientController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Catalog()
+        {
+            return View();
+        }
+
+        public IActionResult Home()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Web_music_feb-jun2024/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Web_music_feb_jun2024.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
